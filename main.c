7cc257f8629c292ba9417bb61e8aaf78d7f4e6b1
Pavan Salunke <pavansalunke96@gmail.com>
2025-09-18 15:13:17 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void swapreference(int *x, int *y){
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+    printf("inside swapreference - x: %d, y: %d\n", *x, *y);
+
+}
+
+int main(){
+    int a,b ;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Before swapreference - a: %d, b: %d\n", a, b);
+    swapreference(a, b);
+    printf("After swapreference - a: %d, b: %d\n", a, b);
+
+    return 0;
+}
